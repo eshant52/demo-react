b1e44a8ddf5d65c5ec0226059d6ade5b214b7726
eshant52 <eshantsonune52@gmail.com>
2023-04-12 12:41:43 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-  const name = 'eshant sonune'
 
   return (
     <div className="App">
@@ -17,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>{name}</h1>
+      <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
